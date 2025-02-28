9d364d21753fba23e7ebe1555187bc7028f94ba2
temurbek2310 <ghostwire2010@gmail.com>
2025-02-28 06:46:20 +0500

    commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 "use client";
-import HeroSection from "./components/hero";
-import Maps from "./components/maps";
-import Navbar from "./components/navbar";
-import Timeline from "./components/timeline";
-import AmirTemur3D from "./components/amir-temur3d";
+// import HeroSection from "./components/hero";
+// import Maps from "./components/maps";
+// import Navbar from "./components/navbar";
+// import Timeline from "./components/timeline";
+// import AmirTemur3D from "./components/amir-temur3d";
+import dynamic from "next/dynamic";
+const AmirTemur3D = dynamic(() => import("../components/AmirTemur3D"), { ssr: false });
+const HeroSection = dynamic(() => import("../components/HeroSection"), { ssr: false });
+const Maps = dynamic(() => import("../components/Maps"), { ssr: false });
+const Navbar = dynamic(() => import("../components/Navbar"), { ssr: false });
+const Timeline = dynamic(() => import("../components/Timeline"), { ssr: false });
 
 export default function Home() {
   return (
