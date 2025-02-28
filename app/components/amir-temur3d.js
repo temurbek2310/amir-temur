6c0253013eaf0f3bd80f8284fdee52081e213f98
temurbek2310 <ghostwire2010@gmail.com>
2025-02-28 06:34:15 +0500

    commit

--- app/components/amir-temur3d.js
+++ app/components/amir-temur3d.js
@@ -1,20 +1,31 @@
+"use client";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
+import { useMemo } from "react";
 
 export default function AmirTemur3D() {
   const { scene } = useGLTF("/amir_temur_statue.glb"); // Model joylashgan joy
+  const optimizedScene = useMemo(() => scene, [scene]);
 
   return (
     <div className="bg-gray-900 text-white py-16 px-6">
       <h2 className="text-4xl font-bold text-center mb-10">
         Amir Temur 3D Haykali
       </h2>
-      <div className="max-w-4xl mx-auto max-h-96">
-        <Canvas camera={{ position: [0, 2, 5] }}>
+      <div className="max-w-4xl mx-auto h-96">
+        <Canvas camera={{ position: [0, 4, 10] }}>
+          {" "}
+          {/* Kamerani uzoqroqqa qo'ydik */}
           <ambientLight intensity={1} />
           <directionalLight position={[5, 5, 5]} />
-          <primitive object={scene} scale={1.5} />
-          <OrbitControls enableZoom={true} />
+          <primitive object={optimizedScene} scale={1} />{" "}
+          {/* Modelni biroz kichikroq qildik */}
+          <OrbitControls
+            enableZoom={true}
+            enableDamping={true}
+            dampingFactor={0.1}
+            rotateSpeed={0.7}
+          />
         </Canvas>
       </div>
     </div>
